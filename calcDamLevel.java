6f9059b41fea51a00146838c725142bdf9cba0ad
Rebecca Reid <a.rebecca.reid@gmail.com>
2019-02-16 16:21:25 -0700

    started kinda coding

---
+++ calcDamLevel.java
@@ -0,0 +1,11 @@
+Calculate New Dam Level Pseudocode:
+class Data{
+  double myRainfall;;
+  double upstreamFlow;
+  double level;
+}
+calcNewDamLevel(Data dailyData){
+  Data previousTen = getPreviousTenDatData();
+  Data averageDay = get
+
+}
