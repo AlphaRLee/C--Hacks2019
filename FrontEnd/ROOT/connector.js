266cef370070c8ea8e6f987086ed3d6098d785c5
R Lee <alpha.r.lee.dev@gmail.com>
2019-02-17 03:00:34 -0700

    INCOMPLETE Integrating in server backend fully

--- FrontEnd/ROOT/connector.js
+++ FrontEnd/ROOT/connector.js
@@ -1,5 +1,12 @@
 function testAjax() { // FIXME Deleteme
-    $.get("server", {requestName: "getDate"}, function(data, status) {
+    $.get("server", {
+        requestName: "stuff",
+        day: "17",
+        month: "1",
+        year: "2019",
+        wLevel: "1075",
+        flow: "60"
+    }, function(data, status) {
         console.log("!!! AJAX RESPONSE: " + data);
         $("#output").text(data);
     });
