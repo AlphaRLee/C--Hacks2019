57d78c1821433db005a226a15e7ad849f936f2c4
Alex Mah <mahalex88@gmail.com>
2019-02-16 22:44:57 -0700

    Got rid of comments

--- FrontEnd/ROOT/testbed/tests/testLiquidTimer.js
+++ FrontEnd/ROOT/testbed/tests/testLiquidTimer.js
@@ -18,98 +18,12 @@ function TestLiquidTimer() {
   shape.CreateLoop();
   ground.CreateFixtureFromShape(shape, 0.0);
 
-/*
-  psd = new b2ParticleSystemDef();
-  psd.radius = 0.025;
-  var particleSystem = world.CreateParticleSystem(psd);
-  */
 
-/*
-  var psd = new b2ParticleSystemDef();
-  psd.radius = 0.025;
-  var particleSystem = world.CreateParticleSystem(psd);
-
-//Sets the box as the center of the simulation
-  shape = new b2PolygonShape;
-  shape.SetAsBoxXYCenterAngle(2, 0.4, new b2Vec2(3, 3.6), 0);
-
-
-  var pd = new b2ParticleGroupDef;
-  pd.flags = b2_tensileParticle | b2_viscousParticle;
-  pd.shape = shape;
-  particleSystem.CreateParticleGroup(pd);
-  */
 
 
   bd = new b2BodyDef;
   var body = world.CreateBody(bd);
 
-
-/*
-//Line to the left (Left and Right co-ords)
-  shape = new b2EdgeShape;
-  shape.Set(new b2Vec2(-2, 3.2), new b2Vec2(-1.2, 3.2));
-  body.CreateFixtureFromShape(shape, 0.1);
-
-//Line to the right
-  bd = new b2BodyDef;
-  body = world.CreateBody(bd);
-  shape = new b2EdgeShape;
-  shape.Set(new b2Vec2(-1.1, 3.2), new b2Vec2(2, 3.2));
-  body.CreateFixtureFromShape(shape, 0.1);
-
-//Line to the left and Vertical
-  bd = new b2BodyDef;
-  body = world.CreateBody(bd);
-  shape = new b2EdgeShape;
-  shape.Set(new b2Vec2(-1.2, 3.2), new b2Vec2(-1.2, 2.8));
-  body.CreateFixtureFromShape(shape, 0.1);
-
-//Line to the right and Vertical
-  bd = new b2BodyDef;
-  body = world.CreateBody(bd);
-  shape = new b2EdgeShape;
-  shape.Set(new b2Vec2(-1.1, 6), new b2Vec2(-1.1, 2.8));
-   body.CreateFixtureFromShape(shape, 0.1);
-
-  bd = new b2BodyDef;
-  body = world.CreateBody(bd);
-  shape = new b2EdgeShape;
-  shape.Set(new b2Vec2(-1.6, 2.4), new b2Vec2(0.8, 2));
-  body.CreateFixtureFromShape(shape, 0.1);
-
-  bd = new b2BodyDef;
-  body = world.CreateBody(bd);
-  shape = new b2EdgeShape;
-  shape.Set(new b2Vec2(1.6, 1.6), new b2Vec2(-0.8, 1.2));
-  body.CreateFixtureFromShape(shape, 0.1);
-
-
-  bd = new b2BodyDef;
-  body = world.CreateBody(bd);
-  shape = new b2EdgeShape;
-  shape.Set(new b2Vec2(-1.2, 0.8), new b2Vec2(-1.2, 0));
-  body.CreateFixtureFromShape(shape, 0.1);
-
-  bd = new b2BodyDef;
-  body = world.CreateBody(bd);
-  shape = new b2EdgeShape;
-  shape.Set(new b2Vec2(-0.4, 0.8), new b2Vec2(-0.4, 0));
-  body.CreateFixtureFromShape(shape, 0.1);
-
-
-  bd = new b2BodyDef;
-  body = world.CreateBody(bd);
-  shape = new b2EdgeShape;
-  shape.Set(new b2Vec2(0.4, 0.8), new b2Vec2(0.4, 0));
-  body.CreateFixtureFromShape(shape, 0.1);
-
-  bd = new b2BodyDef;
-  body = world.CreateBody(bd);
-  shape = new b2EdgeShape;
-  shape.Set(new b2Vec2(1.2, 0.8), new b2Vec2(1.2, 0));
-  body.CreateFixtureFromShape(shape, 0.1);
-  */
 
   /*  Outline of Dam - Rectangle*/
 bd = new b2BodyDef;
@@ -136,23 +50,6 @@ shape = new b2EdgeShape;
 shape.Set(new b2Vec2(-0.25,0.1), new b2Vec2(0.25, 0.1));
 body.CreateFixtureFromShape(shape, 0.1);
 
-//Gate
-/*
-bd = new b2BodyDef;
-body = world.CreateBody(bd);
-shape = new b2EdgeShape;
-shape.Set(new b2Vec2(-0.25,0), new b2Vec2(-0.25, 0.25));
-body.CreateFixtureFromShape(shape, 0.1);
-*/
-
-//Direct liquid to right
-/*
-bd = new b2BodyDef;
-body = world.CreateBody(bd);
-shape = new b2EdgeShape;
-shape.Set(new b2Vec2(-2,4), new b2Vec2(-0.25, 3));
-body.CreateFixtureFromShape(shape, 0.1);
-*/
 
 var psd;
 psd = new b2ParticleSystemDef();
@@ -162,21 +59,6 @@ var particleSystem = world.CreateParticleSystem(psd);
 // Continually create new particals
 window.setInterval(createParticals, 500, particleSystem);
 
-/*
-var i;
-for(i = 0; i < 10000000; i++){
-
-}
-
-shape = new b2PolygonShape;
-shape.SetAsBoxXYCenterAngle(2, 0.4, new b2Vec2(1, 1.2), 0);
-
-
-var pd = new b2ParticleGroupDef;
-pd.flags = b2_tensileParticle | b2_viscousParticle;
-pd.shape = shape;
-particleSystem.CreateParticleGroup(pd);
-*/
 
 
 }
